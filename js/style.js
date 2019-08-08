d3bef1d328b03b25ccead3817b844c1827d0287b
lethach <lethach150699@gmail.com>
2019-08-08 14:08:44 +0700

    go-to-top

--- js/style.js
+++ js/style.js
@@ -3,6 +3,7 @@
     margin:10,
     dots:false,
     animateOut: 'fadeOut',
+    autoplay: true,
     nav:true,
     responsive:{
         0:{
@@ -16,3 +17,33 @@
         }
     }
 })
+// scroll top
+$(document).ready(function(){
+    // hide #go-top first
+    $("#go-top").hide();
+ 
+    // fade in #go-top
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 90) {
+                $('#go-top').fadeIn();
+            } else {
+                $('#go-top').fadeOut();
+            }
+        });
+
+        // scroll body to 0px on click
+        $('#go-top').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+    });
+
+   //tab
+    $(".nav-tabs li").click(function(){
+        $(this).tab('show');
+    });
+ 
+});
